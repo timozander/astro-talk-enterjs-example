7f8735babc2c869214fc88637ba1662972270346
Timo Zander <timo---zander@live.de>
2023-06-05 09:06:25 +0200

    05-start

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,20 +4,11 @@ const blogCollection = defineCollection({
   type: "content", // oder 'data'
   schema: z.object({
     title: z.string(),
-    author: reference("authors"),
     tags: z.array(z.string()),
     image: z.string().optional(),
   }),
 });
 
-export const authorsCollection = defineCollection({
-  type: "data",
-  schema: z.object({
-    name: z.string(),
-  }),
-});
-
 export const collections = {
   blog: blogCollection,
-  authors: authorsCollection,
 };
